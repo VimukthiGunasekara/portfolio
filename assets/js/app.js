90262239953f68e5df15b871bf2429775a5ed0d9
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2024-03-08 19:43:39 -0500

    change theme

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,29 @@
+// rotate text
+let text = document.querySelector(".text p");
+
+text.innerHTML = text.innerHTML.split("").map((char,i)=>
+    `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
+).join("");
+
+// change color
+const colorCheckbox = document.getElementById('colorCheckbox');
+
+colorCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+        document.documentElement.style.setProperty('--primary-color', '#f7f9f9'); 
+        document.documentElement.style.setProperty('--secondary-color', '#23283e'); 
+        document.documentElement.style.setProperty('--background-color', '#d9e8e6'); 
+        document.documentElement.style.setProperty('--yellow-color', '#58a4b0'); 
+        document.documentElement.style.setProperty('--white-color', '#23283e');         
+    } else {
+        document.documentElement.style.setProperty('--primary-color', '#23283e');
+        document.documentElement.style.setProperty('--secondary-color', '#58a4b0');        
+        document.documentElement.style.setProperty('--background-color', '#303755');
+        document.documentElement.style.setProperty('--yellow-color', '#dac24d');        
+        document.documentElement.style.setProperty('--white-color', '#f7f9f9');
+    }
+});
+
 const filterButtons = document.querySelectorAll("#filter-buttons button");
 const filterableCards = document.querySelectorAll("#filterable-cards .card");
 // Function to filter cards based on filter buttons
