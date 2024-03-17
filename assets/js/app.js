1294120567f5a5b30ace720faf9bb79d429b9539
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2024-03-17 10:58:20 -0400

    add scroll animi

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,20 @@
+// scroll animi
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('scroll-animation');
+      }
+    });
+  });
+  
+  const viewbox = document.querySelectorAll('.scroll-animi');
+  viewbox.forEach(image => {
+    observer.observe(image);
+  });
+
 // rotate text
 let text = document.querySelector(".text p");
 
